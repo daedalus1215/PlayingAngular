070d9bb5a31de83c042b7f669bbbcddacba1d385
Laurence Adams <ladams1776@yahoo.com>
2017-07-04 11:49:02 -0400

    Built first component of this project.

---
+++ src/app/courses-component/courses.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+    selector: "courses",
+    template: "./courses.component.html"
+})
+export class CoursesComponent {
+    title: "Courses"
+}
