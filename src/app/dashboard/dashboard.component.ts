e179f2d35c58654fb91d52c3df3f78a8da720e0a
Laurence Adams <ladams1776@yahoo.com>
2017-07-16 10:32:53 -0400

    Connecting to a Web Service with Observable pattern.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { StocksService } from '../stocks/stocks-service/stocks.service';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private stocksService: StocksService) {}   
 
   ngOnInit() {
+    this.getAllStocks();
   }
-
+  
+  getAllStocks() {
+    this.stocksService.getStocksAPI()
+    .subscribe(
+      data => console.log(JSON.stringify(data)),
+      error => console.log('Server Error')
+    );
+  }
 }
